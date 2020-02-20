25c16c22bb0673ae1ec8bf019d8057ed51d32fa1
dotoby <dotoby@gmail.com>
2020-02-20 03:05:08 -0500

    this one has jumpscare lol

---
+++ Assets/Script/OpeningCutSceneTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningCutSceneTimer : MonoBehaviour
+{
+        public string targetScene;
+        public float delay;
+
+        void Start()
+        {
+            StartCoroutine(LoadLevelAfterDelay(delay));
+        }
+
+        IEnumerator LoadLevelAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            Application.LoadLevel(targetScene);
+        }
+}
---
+++ Assets/Script/triggerjump.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerjump : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+            myAnimationController.SetBool("Trigger", true);
+    }
+}
